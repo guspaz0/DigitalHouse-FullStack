1b8efb59d775c3d55781c91d70f133200fe0a2f5
Gustavo Rodolfo Paz <gusti.paz@gmail.com>
2023-09-26 16:58:59 -0300

    actualizo

---
+++ clase10/datosBici.js
@@ -0,0 +1,9 @@
+const bicicletas = require('./bicicletas.json');
+
+//console.log(bicicletas)
+
+let stringBicis = JSON.stringify(bicicletas);
+
+console.log(stringBicis)
+
+module.exports = stringBicis
