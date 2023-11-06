1f0155c2c46aa1cce955eef6e44ad7b6ed5b984b
guspaz0 <guspaz0>
2023-11-05 23:48:22 -0300

    ok

--- clase_17/app.js
+++ clase_17/app.js
@@ -13,6 +13,14 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'/views/home.html'))
 });
 
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+});
+
 app.listen(PORT,(req,res) => {
     console.log(`Server corriendo en puerto http://${HOST}:${PORT}`)
 });
